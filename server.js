57a01dad303f50b9397222ae6fa64f93042853d4
diannaqui <qui22001@byui.edu>
2024-01-16 14:42:48 -0700

    Adding POST - PUT - DELETE

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app.use(bodyParser.json());
+app.use('/', require('./routes')); 
+
 
 mongodb.initDb((err) => {
     if (err) {
